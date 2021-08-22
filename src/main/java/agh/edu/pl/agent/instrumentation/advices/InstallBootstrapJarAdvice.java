93b285290b8badf59bbc4ff0a9be228f58a2c199
enkelian <aniano13@gmail.com>
2021-08-22 13:21:11 +0200

    add spock dependencies

--- src/main/java/agh/edu/pl/agent/instrumentation/advices/InstallBootstrapJarAdvice.java
+++ src/main/java/agh/edu/pl/agent/instrumentation/advices/InstallBootstrapJarAdvice.java
@@ -2,19 +2,19 @@
 package agh.edu.pl.agent.instrumentation.advices;
 
 import io.opentelemetry.javaagent.StaticInstrumenter;
-import java.lang.instrument.Instrumentation;
 import net.bytebuddy.asm.Advice;
+
+import java.lang.instrument.Instrumentation;
 
 public class InstallBootstrapJarAdvice {
 
-  @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
-  static void exit(
-      @Advice.Origin String origin,
-      @Advice.Origin("#t #m") String detailedOrigin,
-      @Advice.Argument(value = 0, readOnly = false) Instrumentation inst) {
+    @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
+    static void exit(
+            @Advice.Origin("#t #m") String detailedOrigin,
+            @Advice.Argument(value = 0, readOnly = false) Instrumentation inst) {
 
-    System.out.println("[INSTRUMENTATION] ENTER " + detailedOrigin);
+        System.out.println("[INSTRUMENTATION] ENTER " + detailedOrigin);
 
-    inst.addTransformer(StaticInstrumenter.getPreTransformer());
-  }
+        inst.addTransformer(StaticInstrumenter.getPreTransformer());
+    }
 }
---
+++ src/test/groovy/agh/edu/pl/agent/instrumentation/JarUtilsTest.groovy
@@ -0,0 +1,13 @@
+package agh.edu.pl.agent.instrumentation
+
+import spock.lang.Specification
+
+class JarUtilsTest extends Specification {
+
+    def "test copying file"() {
+        expect:
+        1 + 1 == 2
+    }
+
+
+}
