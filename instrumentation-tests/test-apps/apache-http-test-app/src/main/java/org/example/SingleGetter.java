d5673a69a9003790b17eb37af8dc1de559325ca8
enkelian <aniano13@gmail.com>
2021-11-12 11:57:03 +0100

    change spotless logic

--- instrumentation-tests/test-apps/apache-http-test-app/src/main/java/org/example/SingleGetter.java
+++ instrumentation-tests/test-apps/apache-http-test-app/src/main/java/org/example/SingleGetter.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.IOException;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -9,37 +10,34 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
-import java.io.IOException;
-
 public class SingleGetter {
 
-    public static void main(String[] args) {
-        try {
-            makeCall();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public static void main(String[] args) {
+    try {
+      makeCall();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    private static void makeCall() throws IOException {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet request = new HttpGet("https://httpbin.org/get");
+  private static void makeCall() throws IOException {
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpGet request = new HttpGet("https://httpbin.org/get");
 
-            // add request headers
-            request.addHeader("custom-key", "mkyong");
-            request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
+      // add request headers
+      request.addHeader("custom-key", "mkyong");
+      request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
 
-            try (CloseableHttpResponse response = httpClient.execute(request)) {
-                HttpEntity entity = response.getEntity();
-                if (entity != null) {
-                    JSONObject result = new JSONObject(EntityUtils.toString(entity));
-                    if (result.getJSONObject("headers").has("Traceparent")) {
-                        System.exit(0);
-                    }
-                }
-            }
+      try (CloseableHttpResponse response = httpClient.execute(request)) {
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+          JSONObject result = new JSONObject(EntityUtils.toString(entity));
+          if (result.getJSONObject("headers").has("Traceparent")) {
+            System.exit(0);
+          }
         }
-        System.exit(1);
+      }
     }
-
+    System.exit(1);
+  }
 }
