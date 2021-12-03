e68b3cd049b4aa6d8d1b727771b183495fe39d69
Kamil Krzempek <kamilkrzempek99@gmail.com>
2021-12-03 23:05:51 +0100

    convert apache http app test to use fake backend

--- instrumentation-tests/scripts/test-apache-http-app.sh
+++ instrumentation-tests/scripts/test-apache-http-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mvn --projects instrumentation-tests/test-apps/spring-test-app package
+mvn --projects instrumentation-tests/test-apps/apache-http-test-app package
 
 java -jar instrumentation-tests/utils/fake-backend/target/fake-backend-1.0-SNAPSHOT-shaded.jar &
 BACKEND_PID=$!
@@ -8,15 +8,13 @@ BACKEND_PID=$!
 export OTEL_TRACES_EXPORTER=otlp
 export OTEL_EXPORTER_OTLP_ENDPOINT=http://127.0.0.1:8888/
 
-java -Dio.opentelemetry.javaagent.shaded.io.opentelemetry.context.contextStorageProvider=default -jar instrumentation-tests/test-apps/spring-test-app/target/spring-test-app-0.0.1-SNAPSHOT-instrumented.jar &
-SPRING_PID=$!
 
-sleep 10;
-curl -m 2 http://localhost:8080/greeting
-sleep 10;
+java -Dio.opentelemetry.javaagent.shaded.io.opentelemetry.context.contextStorageProvider=default -jar ./instrumentation-tests/test-apps/apache-http-test-app/target/apache-http-test-app-1.0-SNAPSHOT-shaded-instrumented.jar
+APP_PID=$!
+
 TRACES=$(curl -m 2 http://localhost:8888/get-traces)
 
-kill -9 $SPRING_PID
+kill -9 $APP_PID
 kill -9 $BACKEND_PID
 
 if [ "$TRACES" != "[]" ]; then
